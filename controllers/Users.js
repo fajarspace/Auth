cb935e663b92fd1dda410ffd392263a2e2479741
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-15 19:45:52 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Tue Nov 15 19:45:52 WIB 2022

--- controllers/Users.js
+++ controllers/Users.js
@@ -21,8 +21,23 @@ export const getUserById = async(req, res) => {
         res.status(500).json({msg: error.message});
     }
 }
-export const createUser = (req, res) => {
-    
+export const createUser = async(req, res) => {
+    const {name, email, password, confPassword, role} = req.body;
+    if (password !== confPassword) {
+        return res.status(400).json({msg: "password & confirm password tidak cocok"})
+    }
+    const hashPassword = await argon2.hash(password);
+    try {
+        await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        });
+        res.status(201).json({msg: "register berhasil!"})
+    } catch (error) {
+        return res.status(400).json({msg: error.message})
+    }
 }
 export const updateUser = (req, res) => {
     
--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 git add .
 
-msg="rebuilding site ✧*｡٩(ˊᗜˋ*)و✧*｡ `date`"
+msg="✧*｡٩(ˊᗜˋ*)و✧*｡ `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
