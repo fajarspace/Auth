05edae128ac1f012cf367beb206b097db0a1ef1f
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-15 21:27:18 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Tue Nov 15 21:27:18 WIB 2022

--- controllers/Users.js
+++ controllers/Users.js
@@ -3,7 +3,9 @@ import argon2 from "argon2";
 
 export const getUsers = async(req, res) => {
     try {
-        const response = await User.findAll();
+        const response = await User.findAll({
+            attributes: ['uuid','name','email','role']
+        });
         res.status(200).json(response);
     } catch (error) {
         res.status(500).json({msg: error.message});
@@ -12,6 +14,7 @@ export const getUsers = async(req, res) => {
 export const getUserById = async(req, res) => {
     try {
         const response = await User.findOne({
+            attributes: ['uuid','name','email','role'],
             where: {
                 uuid: req.params.id
             }
@@ -38,8 +41,38 @@ export const createUser = async(req, res) => {
         return res.status(400).json({msg: error.message});
     }
 }
-export const updateUser = (req, res) => {
-    
+export const updateUser = async(req, res) => {
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "user tidak ditemukan"});
+    const {name, email, password, confPassword, role} = req.body;
+    let hashPassword;
+    if(password === "" || password === null){
+        hashPassword = user.password
+    }else{
+        hashPassword = await argon2.hash(password);
+    }
+    if (password !== confPassword) return res.status(400).json({msg: "password & confirm password tidak cocok"});
+    try {
+        await User.update({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        },{
+            where: {
+                id: user.id
+            }
+        });
+        res.status(201).json({msg: "register berhasil!"});
+    } catch (error) {
+        return res.status(400).json({msg: error.message});
+    }
+
+
 }
 export const deleteUser = (req, res) => {
     
