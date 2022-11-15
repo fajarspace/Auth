c0b80a207da4b5d172accc2b959b0352aee01910
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-15 22:18:45 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Tue Nov 15 22:18:45 WIB 2022

---
+++ controllers/Auth.js
@@ -0,0 +1,27 @@
+import User from "../models/UsersModel.js";
+import argon2 from "argon2";
+
+export const login = async(req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: req.body.username
+        }
+    });
+    if(!user) return res.status(404).json({msg: "user tidak ditemukan"});
+    const validasiPass = await argon2.verify(user.password, req.body.password);
+    if(!validasiPass) return res.status(400).json({msg: "password salah!"});
+    req.session.userId = user.uuid;
+    const uuid = user.uuid;
+    const name = user.name;
+    const email = user.email;
+    const username = user.username;
+    const role = user.role;
+    res.status(200).json({uuid, name, email, username, role})
+}
+
+export const logOut = (req, res) => {
+    req.session.destroy((err) =>{
+        if(err) return res.status(400).json({msg: "tidak dapat logout"});
+        res.status(200).json({msg: "anda telah logout"});
+    });
+}
