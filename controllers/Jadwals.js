1abb84d49f747c3beda6a4a7b3947f8d799317ba
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-19 14:28:12 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Sat Nov 19 14:28:12 WIB 2022

--- controllers/Jadwals.js
+++ controllers/Jadwals.js
@@ -115,5 +115,30 @@ export const updateJadwal = async(req, res) =>{
 }
 
 export const deleteJadwal = async(req, res) =>{
-   
+    try {
+        const jadwal = await Jadwal.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!jadwal) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {asisten, hari, kelas, waktu} = req.body;
+        if(req.role === "instruktur"){
+            await Jadwal.destroy({
+                where: {
+                    id: jadwal.id
+                }
+            })
+        }else{
+            if(req.userId !== jadwal.userId) return res.status(403).json({msg: "akses tertolack!"});
+            await Jadwal.destroy({
+                where:{
+                    [Op.and]:[{id: jadwal.id}, {userId: req.userId}]
+                }
+            })
+        }
+        res.status(200).json({msg: "jadwal deleted!"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
