ffb2b582ce8a7905c26fc6e3003e56976d528219
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-11-19 10:14:06 +0700

    ✧*｡٩(ˊᗜˋ*)و✧*｡ Sat Nov 19 10:14:05 WIB 2022

--- controllers/Jadwals.js
+++ controllers/Jadwals.js
@@ -1,17 +1,56 @@
 import Jadwal from "../models/JadwalModel.js";
+import User from "../models/UsersModel.js";
+import {Op} from "sequelize";
 
-export const getJadwals = (req, res) => {
+export const getJadwals = async (req, res) =>{
+    try {
+        let response;
+        if(req.role === "instruktur"){
+            response = await Jadwal.findAll({
+                include:[{
+                    model: User
+                }]
+            });
+        }else{
+            response = await Jadwal.findAll({
+                where:{
+                    userId: req.userId
+                },
+                include:[{
+                    model: User
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
 
+export const getJadwalById = async(req, res) =>{
+   
 }
-export const getJadwalById = (req, res) => {
+
+export const createJadwal = async(req, res) =>{
+    const {asisten, hari, kelas, waktu} = req.body;
+    try {
+        await Jadwal.create({
+            asisten: asisten,
+            hari: hari,
+            kelas: kelas,
+            waktu: waktu,
+            userId: req.userId
+        });
+        res.status(201).json({msg: "Jadwal Created Successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+}
+
+export const updateJadwal = async(req, res) =>{
     
 }
-export const createJadwal = (req, res) => {
-    
-}
-export const updateJadwal = (req, res) => {
-    
-}
-export const deleteJadwal = (req, res) => {
-    
+
+export const deleteJadwal = async(req, res) =>{
+   
 }
--- routes/JadwalRoute.js
+++ routes/JadwalRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getJadwals, getJadwalById, createJadwal, updateJadwal, deleteJadwal } from "../controllers/Jadwals.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/jadwals', getJadwals);
-router.get('/jadwals/:id', getJadwalById);
-router.post('/jadwals', createJadwal);
-router.patch('/jadwals/:id', updateJadwal);
-router.delete('/jadwals/:id', deleteJadwal);
+router.get('/jadwals', verifyUser, getJadwals);
+router.get('/jadwals/:id', verifyUser, getJadwalById);
+router.post('/jadwals', verifyUser, createJadwal);
+router.patch('/jadwals/:id', verifyUser, updateJadwal);
+router.delete('/jadwals/:id', verifyUser, deleteJadwal);
 
 export default router;
